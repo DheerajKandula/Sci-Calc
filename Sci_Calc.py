a616928a98629f3fd3e63ca428a557312b6d231d
DheerajKandula <dheerajkandula2008@gmail.com>
2026-06-04 12:59:52 +0530

    Added Logarithm with custom base.
    WIP :- Trigonometric functions.
    
    Also will polish the questions after all the functions are added.

--- Sci_Calc.py
+++ Sci_Calc.py
@@ -1,10 +1,10 @@
 e = 2.71828
 π = 3.14159
 
-print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Floor Division\n7.Exponentiation\n8.Root\n9.e Power\n10.10 Power\n11.Factorial\n12.Percentage")
+print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Floor Division\n7.Exponentiation\n8.Root\n9.e Power\n10.10 Power\n11.Factorial\n12.Percentage\n13.Logarithm")
 c = int(input("Enter the S.No of the Operation you want to use: "))
 a = float(input("Enter first number: "))
-if(c<=8 | c==12):
+if(c<=8 | c>=12):
     b = float(input("Enter second number: "))
 
 def Factorial(a):
@@ -14,6 +14,16 @@ def Factorial(a):
 
 def Percentage(a,b):
     return (a/100)*b
+
+def Logarithm(a,b):
+    d = 1
+    e = 1
+    f = b
+    while(f<a):
+        e=e*b
+        f=f*b
+        d=d+1
+    return d
 
 match c:
     case 1: print(a+b)
@@ -28,4 +38,5 @@ match c:
     case 10: print(10**a)
     case 11: print(Factorial(a))
     case 12: print(f"{Percentage(a,b)}%")
+    case 13: print(Logarithm(a,b))
     case _: print("Nothing was selected")
