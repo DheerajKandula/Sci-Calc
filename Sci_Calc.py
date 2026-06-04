745d17830fcc91d529b57d2e8366570dd349aab3
DheerajKandula <dheerajkandula2008@gmail.com>
2026-06-04 21:05:11 +0530

    Changed the Input
    Polished up the input system in relation to the given operation.
    
    WIP :- Inverse Trigonometric Functions.

--- Sci_Calc.py
+++ Sci_Calc.py
@@ -2,10 +2,33 @@ e = 2.71828
 π = 3.14159
 
 print("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulus\n6.Floor Division\n7.Exponentiation\n8.Root\n9.e Power\n10.10 Power\n11.Factorial\n12.Percentage\n13.Logarithm\n14.Sine\n15.Cosine\n16.Tangent")
+
 c = int(input("Enter the S.No of the Operation you want to use: "))
-a = float(input("Enter first number: "))
-if(c<=8 | c==12 | c==13):
-    b = float(input("Enter second number: "))
+
+if(c==4 or c==5 or c==6):
+    a=float(input("Enter the Dividend:"))
+    b=float(input("Enter the Divisor:"))
+elif(c==7):
+    a=float(input("Enter the base:"))
+    b=float(input(("Enter the power:")))
+elif(c==8):
+    a=float(input("Enter the Base:"))
+    b=float(input("Enter the Root:"))
+elif(c==9 or c==10):
+    a=float(input("Enter the Power:"))
+elif(c==11):
+    a=float(input("Enter the Number:"))
+elif(c==12):
+    a=float(input("How much Percentage:"))
+    b=float(input("Percentage of which Number:"))
+elif(c==13):
+    a=float(input("Enter the Number:"))
+    b=float(input("Enter the base for the Logarithm:"))
+elif(c==14 or c==15 or c==16):
+    a=float(input("Enter the angle in Degrees:"))
+elif(c==1 or c==2 or c==3):
+    a=float(input("Enter the First Number:"))
+    b=float(input("Enter the Second Number:"))
 
 def Factorial(a):
     if a==0 or a==1:
